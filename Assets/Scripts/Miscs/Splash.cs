d789dd95b04786d3688f45980250a93b35e6aed4
supamiu <m1uu@hotmail.fr>
2016-03-22 21:31:27 +0100

    Ajout des trois scènes de base + splash screen + menu avec bouton de start lié à la scène Game

---
+++ Assets/Scripts/Miscs/Splash.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour {
+	//Seconds to wait on splash
+	int delay = 2;
+	//Just a private field to store actual splash end time
+	float endTime;
+
+	// Use this for initialization
+	void Start () {
+		this.endTime = Time.time + delay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > endTime) {
+			OpenMenu ();
+		}
+	}
+
+	void OpenMenu(){
+		SceneManager.LoadScene ("Menu");
+	}
+}
