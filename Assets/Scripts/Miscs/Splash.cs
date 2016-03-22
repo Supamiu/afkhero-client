7f782b638b7c1b4ddb1a09a9c0a6ce443224b4b6
supamiu <m1uu@hotmail.fr>
2016-03-22 21:34:24 +0100

    Petit fix niveau propreté du script pour le Splash

--- Assets/Scripts/Miscs/Splash.cs
+++ Assets/Scripts/Miscs/Splash.cs
@@ -4,20 +4,11 @@ using UnityEngine.SceneManagement;
 
 public class Splash : MonoBehaviour {
 	//Seconds to wait on splash
-	int delay = 2;
-	//Just a private field to store actual splash end time
-	float endTime;
+	int duration = 2;
 
 	// Use this for initialization
 	void Start () {
-		this.endTime = Time.time + delay;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Time.time > endTime) {
-			OpenMenu ();
-		}
+		Invoke (OpenMenu, (float)duration);
 	}
 
 	void OpenMenu(){
